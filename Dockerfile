fda60c822288c703ac7a95b018c4cccaf4d2a582
Sven Latham <sven@noggin.bi>
2023-11-12 14:42:26 +0000

    Removed redundant files Shifted content to html/

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM cr.brightbox.com/acc-uww2d/sven/www-basic
+COPY . /var/www/html/
---
+++ build.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build -t cr.brightbox.com/acc-uww2d/sven/noggin-ltd-uk . && docker push cr.brightbox.com/acc-uww2d/sven/noggin-ltd-uk
