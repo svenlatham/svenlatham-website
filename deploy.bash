14b6f9c83bcdc3187eae12a02560f4b38d405fe4
Sven Latham <git@svenlatham.com>
2025-03-24 19:20:18 +0000

    Switched podman to docker

--- deploy.bash
+++ deploy.bash
@@ -1,19 +1,12 @@
 #!/bin/bash
 
-# Use podman in place of docker if available
-if command -v podman &> /dev/null
-then
-    BUILDER=podman
-else
-    BUILDER=docker
-fi
 
 # Stage 1 - Build the site
-$BUILDER build -t svenlatham-website . || exit 1
-CONTAINER_ID=$($BUILDER run -d svenlatham-website build --output-path=/build/)
-$BUILDER wait $CONTAINER_ID
-$BUILDER commit $CONTAINER_ID build
+docker build -t svenlatham-website . || exit 1
+CONTAINER_ID=$(docker run -d svenlatham-website build --output-path=/build/)
+docker wait $CONTAINER_ID
+docker commit $CONTAINER_ID build
 
 # Stage 2 - embed the build into nginx and deploy
-$BUILDER build -t cr.brightbox.com/acc-uww2d/sven/svenlatham-website -f deploy/Dockerfile . || exit 1
-$BUILDER push cr.brightbox.com/acc-uww2d/sven/svenlatham-website
+docker build -t cr.brightbox.com/acc-uww2d/sven/svenlatham-website -f deploy/Dockerfile . || exit 1
+docker push cr.brightbox.com/acc-uww2d/sven/svenlatham-website
--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-podman build -t svenlatham-website . || exit 1
-podman run -v ./src/:/src/ -it -p 5000:5000 svenlatham-website server -h 0.0.0.0
+docker build -t svenlatham-website . || exit 1
+docker run -v ./src/:/src/ -it -p 5000:5000 svenlatham-website server -h 0.0.0.0
